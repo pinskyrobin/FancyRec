bb29cc47e08cd7b5d38fce1087dcf39fc2a8715d
pinskyrobin <pinskyrobin@foxmail.com>
2023-02-18 10:51:15 +0800

    add test shell script

--- bin/TEMPLATE_do_test.sh
+++ bin/TEMPLATE_do_test.sh
@@ -5,6 +5,9 @@ logger_name=@@@logger_name@@@
 n_caption=@@@n_caption@@@
 overwrite=@@@overwrite@@@
 
-gpu=2
+checkpoint_name=@@@checkpoint_name@@@
+batch_size=@@@batch_size@@@
 
-CUDA_VISIBLE_DEVICES=$gpu python tester.py $testCollection --rootpath $rootpath --overwrite $overwrite --logger_name $logger_name --n_caption $n_caption
+gpu=0
+
+CUDA_VISIBLE_DEVICES=$gpu python tester.py $testCollection --rootpath $rootpath --overwrite $overwrite --logger_name $logger_name --n_caption $n_caption --checkpoint_name $checkpoint_name --batch_size $batch_size

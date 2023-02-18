177c15f98e58281aa6c78425ea0e4d72e6f281f0
pinskyrobin <pinskyrobin@foxmail.com>
2023-02-18 10:50:59 +0800

    trainable with GPU when batch_size=8

--- bin/public_cluster.sh
+++ bin/public_cluster.sh
@@ -10,7 +10,7 @@ concate=full # full|reduced
 overwrite=1
 num_epochs=100 # use early stopping mechanism
 text_net=bi-gru # bi-gru|transformer
-batch_size=64
+batch_size=8
 metric=auc
 n_caption=1 # how many captions in each video
 learning_rate=0.0001
@@ -23,13 +23,13 @@ common_embedding_size=1024
 margin=0.2
 # final fusion style of Visual and Text
 fusion_style=fc
-workers=0
+workers=8
 brand_num=52
-#resume=/root/VisualSearch/insCartrain/dinner_project/insCarval/dual_encoding_concate_full_dp_0.2_measure_cosine/vocab_word_vocab_5_word_dim_500_text_rnn_size_512_text_norm_True_text_net_bi-gru_kernel_sizes_2-3-4_num_512/visual_feature_resnet152_img_2048_visual_rnn_size_1024_visual_norm_True_kernel_sizes_2-3-4-5_num_512/mapping_text_1024_img_1024/loss_func_eet_margin_0.3_direction_all_max_violation_True_cost_style_sum/optimizer_adam_lr_0.001_decay_0.99_grad_clip_2.0_val_metric_auc/runs_0/model_best.pth.tar
+postfix=public_cluster
 
 cd FGMCD/bin
 # training
-gpu=-1
+gpu=0
 CUDA_VISIBLE_DEVICES=$gpu ../../anaconda3/bin/python3 ../trainer.py $trainCollection $valCollection $testCollection \
                                             --rootpath $rootpath \
                                             --workers $workers \
@@ -44,7 +44,4 @@ CUDA_VISIBLE_DEVICES=$gpu ../../anaconda3/bin/python3 ../trainer.py $trainCollec
                                             --text_mapping_size $text_mapping_size \
                                             --visual_mapping_size $visual_mapping_size --margin $margin \
                                             --fusion_style $fusion_style \
-                                            --max_violation  \
-                                            --single_modal_visual  # use visual only
-# evaluation (Notice that a script file do_test_${testCollection}.sh will be automatically generated when the training process ends.)
-#./do_test_dual_encoding_${testCollection}.sh
+                                            --max_violation --postfix $postfix

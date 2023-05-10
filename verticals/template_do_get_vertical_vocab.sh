ef08a359735bf67517229ac6deca177289fbafb8
PinskyRobin <pinskyrobin@foxmail.com>
2023-05-10 15:35:19 +0800

    del verticals/

--- verticals/template_do_get_vertical_vocab.sh
+++
@@ -1,9 +0,0 @@
-rootpath=/root/VisualSearch/verticals/@@@vertical@@@
-collection=@@@collection@@@
-# msrvtt10ktrain
-threshold=5
-overwrite=1
-for text_style in bow rnn   # 直接就是名字
-do
-/root/anaconda3/bin/python ../util/vocab.py $collection --rootpath $rootpath --threshold $threshold --text_style $text_style --overwrite $overwrite
-done
